12fe7f9038e1a2281cd0624136a27e5c844b7934
ksilex <negative074@gmail.com>
2021-02-19 10:24:24 +0300

    response created

--- time_app.rb
+++ time_app.rb
@@ -1,19 +1,34 @@
-require 'rack'
-
-FORMATS = %w[year month day hour minute second].freeze
+FORMATS = { 'year' => '%Y', 'month' => '%m', 'day' => '%d', 'hour' => '%H',
+            'minute' => '%M', 'second' => '%S' }.freeze
 class TimeApp
   def call(env)
     @request = Rack::Request.new(env)
-    response.finish
+    response_conditions
   end
 
-  def response
-    if @request.path != "/time"
-      Rack::Response.new(['error'], 404, { 'Content-Type' => 'text/plain' })
+  def response_conditions
+    return response(404, 'Path error or time format is nil') if (@request.path != '/time' || user_input.nil?)
+
+    if (user_input - FORMATS.keys).empty?
+      response(200, time_output)
     else
-      @request.params
-      Rack::Response.new(['time'], 200, { 'Content-Type' => 'text/plain' })
+      response(404, "Unknown time format(s): #{user_input - FORMATS.keys}")
     end
   end
 
+  private
+
+  def response(status, content)
+    Rack::Response.new([content], status, { 'Content-Type' => 'text/plain' }).finish
+  end
+
+  def user_input
+    @request.params['format']&.split(',')
+  end
+
+  def time_output
+    output = []
+    user_input.each { |el| output.push(FORMATS[el]) if FORMATS[el] }
+    Time.now.strftime(output.join('-'))
+  end
 end
